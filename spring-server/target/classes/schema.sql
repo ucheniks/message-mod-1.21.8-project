49abb39e43a2a745839986252fe99e71be720c88
ucheniks <shelgaas00@mail.ru>
2025-10-18 03:35:12 +0300

    Move .gitignore to root and clean build files

--- spring-server/target/classes/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS messages (
-    id        SERIAL PRIMARY KEY,
-    uuid      UUID NOT NULL,
-    text      VARCHAR(256) NOT NULL
-);
-
-CREATE INDEX IF NOT EXISTS idx_messages_uuid ON messages(uuid);
